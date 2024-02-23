a3e37855cd8631f5e41e73cb8b45f4dc6b1f0d39
Rishi Kumar <1720744+mrrishimeena@users.noreply.github.com>
2024-02-23 12:24:33 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,6 +76,40 @@ const isDockerEnv = () => {
   return process.env.RUNNING_IN_DOCKER === 'true';
 };
 
+
+
+const v8 = require('v8');
+function bytesToMB(bytes) {
+  return bytes != null ? (bytes / 1024 / 1024).toFixed(2) + ' MB' : 'N/A';
+}
+function readableHeapStats() {
+  const heapStats = v8.getHeapStatistics();
+  const readableStats = {
+    total_heap_size: bytesToMB(heapStats.total_heap_size),
+    total_heap_size_executable: bytesToMB(heapStats.total_heap_size_executable),
+    total_physical_size: bytesToMB(heapStats.total_physical_size),
+    total_available_size: bytesToMB(heapStats.total_available_size),
+    used_heap_size: bytesToMB(heapStats.used_heap_size),
+    heap_size_limit: bytesToMB(heapStats.heap_size_limit),
+    malloced_memory: bytesToMB(heapStats.malloced_memory),
+    peak_malloced_memory: bytesToMB(heapStats.peak_malloced_memory),
+    external_memory: bytesToMB(heapStats.external_memory),
+    // For non-size related or boolean properties, directly copy them or provide default values
+    does_zap_garbage: heapStats.does_zap_garbage != null ? heapStats.does_zap_garbage : 'N/A',
+    number_of_native_contexts: heapStats.number_of_native_contexts != null ? heapStats.number_of_native_contexts : 'N/A',
+    number_of_detached_contexts: heapStats.number_of_detached_contexts != null ? heapStats.number_of_detached_contexts : 'N/A',
+    total_global_handles_size: bytesToMB(heapStats.total_global_handles_size),
+    used_global_handles_size: bytesToMB(heapStats.used_global_handles_size),
+  };
+  console.log(readableStats);
+}
+
+setInterval(function(){
+  readableHeapStats()
+}, 10000)
+
+
+
 console.log(`Running in Docker (env check): ${isDockerEnv()}`);
 
 
